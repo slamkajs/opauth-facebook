f70df6f847a86235c3ba38815f099b0c858bb4a6
slamkajs <slamkajs@github.com>
2013-10-14 21:20:21 -0400

    Updated to universal data return

--- FacebookStrategy.php
+++ FacebookStrategy.php
@@ -78,14 +78,50 @@ class FacebookStrategy extends OpauthStrategy{
 					),
 					'raw' => $me
 				);
+
+				$education = array();
+				$work = array();
+
+				if(isset($me->education) && !empty($me->education)) {
+					foreach ($me->education as $edu => $cnt) {
+						$edu = $me->education[$edu];
+						$field_of_study = array();
+						if(isset($edu->concentration) && !empty($edu->concentration)) {
+							foreach($edu->concentration as $field) {
+								array_push($field_of_study, $field->name);
+							}
+						}
+
+						array_push($education, array(
+							'institution' => (isset($edu->school) && !empty($edu->school) ? $edu->school->name : 'Unknown Institution'),
+							'field_of_study' => implode(', ', $field_of_study),
+							'start_date' => null,
+							'end_date' => (isset($edu->year) && !empty($edu->year) ? date('Y-m-d H:i:s', strtotime('01/01/' . $edu->year->name)) : null),
+							'currentlyAttendingFlag' => (!isset($edu->year) || (isset($edu->year) && date('Y', $edu->year->name) <= date('Y')) ? 1 : 0)));
+					}
+				}
+
+				if(isset($me->work) && !empty($me->work)) {
+					foreach ($me->work as $job => $cnt) {
+						$job = $me->work[$job];
+						array_push($work, array(
+							'employer' => (isset($job->employer) && !empty($job->employer) ? $job->employer->name : 'Unknown Employer'),
+							'position' => (isset($job->position) && !empty($job->position) ? $job->position->name : null),
+							'start_date' => (isset($job->start_date) && !empty($job->start_date) ? $job->start_date : null),
+							'end_date' => (isset($job->end_date) && !empty($job->end_date) && strToTime($job->end_date) != null ? $job->end_date : null),
+							'currentlyWorkingFlag' => (isset($job->end_date) && !empty($job->end_date) && strToTime($job->end_date) != null ? (strToTime(date('Y-m-d')) <= strToTime($job->end_date) ? 1 : 0) : 1)));
+					}
+				}
 				
 				if (!empty($me->email)) $this->auth['info']['email'] = $me->email;
-				if (!empty($me->username)) $this->auth['info']['nickname'] = $me->username;
+				if (!empty($me->username)) $this->auth['info']['username'] = $me->username;
 				if (!empty($me->first_name)) $this->auth['info']['first_name'] = $me->first_name;
 				if (!empty($me->last_name)) $this->auth['info']['last_name'] = $me->last_name;
 				if (!empty($me->location)) $this->auth['info']['location'] = $me->location->name;
 				if (!empty($me->link)) $this->auth['info']['urls']['facebook'] = $me->link;
 				if (!empty($me->website)) $this->auth['info']['urls']['website'] = $me->website;
+				if (!empty($me->education)) $this->auth['info']['education'] = $education;
+				if (!empty($me->work)) $this->auth['info']['work'] = $work;
 				
 				/**
 				 * Missing optional info values
